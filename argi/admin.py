2f824c2d3aa5f8a4c5d0042ab845bc9fa810db31
buni <buni@buni-Latitude-D630.(none)>
2013-10-19 16:06:33 +0300

    add plot to model

--- argi/admin.py
+++ argi/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from argi.models import Organization
+from argi.models import *
 
 admin.site.register(Organization)
+admin.site.register(Plot)
--- argi/models.py
+++ argi/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
-class Organization(models.Model):
-	name = models.CharField(max_length=200)
+class Plot(models.Model):
+    crop = models.CharField(max_length=200)
 
+    def __unicode__(self):
+    	return self.crop
+
+class Organization(models.Model):
+    name = models.CharField(max_length=200)
+    plots = models.ForeignKey(Plot)
+    
+    def __unicode__(self):
+    	return self.name
